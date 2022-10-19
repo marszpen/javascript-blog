9e30158e14d752c050c11460d7832feb99a20e31
Karina Penkala <marsz@poczta.onet.pl>
2022-10-19 12:18:23 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 const titleClickHandler = function(event){
+    const clickedElement = this
     console.log('Link was clicked');
-    console.log(event);
+
      /* remove class 'active' from all article links  */
+     const activeLinks = document.querySelectorAll('.titles a.active');
 
+     for(let activeLink of activeLinks){
+       activeLink.classList.remove('active');
+     }
   /* add class 'active' to the clicked link */
-
+     clickedElement.classList.add('active')
+        console.log('clickedElement:', clickedElement);
   /* remove class 'active' from all articles */
-
-  /* get 'href' attribute from the clicked link */
+        const activeArcticles = document.querySelectorAll('.post active');
+    
+        for (let activeArticle of activeArticles){
+            activeArticle.classList.remove('active');
+        }
+/* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
