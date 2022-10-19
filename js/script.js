3e7cc988363c917954799f32fc943dcf501502b2
Karina Penkala <marsz@poczta.onet.pl>
2022-10-19 20:52:15 +0200

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -23,9 +23,10 @@ const titleClickHandler = function(event){
         const articleSelector = clickedElement.getAttribute('href');
             console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-        const targetArticle = document.querySelector('href');
+        const targetArticle = document.querySelector(articleSelector);
         console.log(targetArticle);
   /* add class 'active' to the correct article */
+        targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
