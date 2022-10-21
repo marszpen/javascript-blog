806892dfe1608dae1a3a6031463d2bcadce3e7d8
Karina Penkala <marsz@poczta.onet.pl>
2022-10-21 20:48:13 +0200

    Transfer titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,40 +1,5 @@
 {
-    const titleClickHandler = function(event){
-    event.preventDefault();
-    const clickedElement = this;
     
-    console.log('Link was clicked');
-
-  /* remove class 'active' from all article links  */
-     const activeLinks = document.querySelectorAll('.titles a.active');
-
-     for(let activeLink of activeLinks){
-       activeLink.classList.remove('active');
-     }
-  /* add class 'active' to the clicked link */
-     clickedElement.classList.add('active')
-        console.log('clickedElement:', clickedElement);
-  /* remove class 'active' from all articles */
-        const activeArticles = document.querySelectorAll('article');
-    
-        for (let activeArticle of activeArticles){
-            activeArticle.classList.remove('active');
-        }
-  /* get 'href' attribute from the clicked link */
-        const articleSelector = clickedElement.getAttribute('href');
-            console.log(articleSelector);
-  /* find the correct article using the selector (value of 'href' attribute) */
-        const targetArticle = document.querySelector(articleSelector);
-        console.log(targetArticle);
-  /* add class 'active' to the correct article */
-        targetArticle.classList.add('active');
-  }
-  
-  const links = document.querySelectorAll('.titles a');
- 
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
 
 //
 
@@ -78,6 +43,45 @@ function generateTitleLinks(){
     console.log(linkHTML);
   }
     titleList.innerHTML = html;
+
+    //
+    const titleClickHandler = function(event){
+        event.preventDefault();
+        const clickedElement = this;
+        
+        console.log('Link was clicked');
+    
+      /* remove class 'active' from all article links  */
+         const activeLinks = document.querySelectorAll('.titles a.active');
+    
+         for(let activeLink of activeLinks){
+           activeLink.classList.remove('active');
+         }
+      /* add class 'active' to the clicked link */
+         clickedElement.classList.add('active')
+            console.log('clickedElement:', clickedElement);
+      /* remove class 'active' from all articles */
+            const activeArticles = document.querySelectorAll('article');
+        
+            for (let activeArticle of activeArticles){
+                activeArticle.classList.remove('active');
+            }
+      /* get 'href' attribute from the clicked link */
+            const articleSelector = clickedElement.getAttribute('href');
+                console.log(articleSelector);
+      /* find the correct article using the selector (value of 'href' attribute) */
+            const targetArticle = document.querySelector(articleSelector);
+            console.log(targetArticle);
+      /* add class 'active' to the correct article */
+            targetArticle.classList.add('active');
+      }
+      
+      const links = document.querySelectorAll('.titles a');
+        console.log(links);
+     
+      for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+      }
 }
 
 
